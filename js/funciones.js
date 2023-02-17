ac2638666634ee9cd78fa2d6329387f35da127ad
juan9900 <juanluislauretta@gmail.com>
2023-02-17 09:53:02 -0400

    full site

---
+++ js/funciones.js
@@ -0,0 +1,54 @@
+//Funciones
+(function($) {
+	"use strict";
+
+	$(".menu-toggle").click(function(e) {
+		e.preventDefault();
+		$("#sidebar-wrapper").toggleClass("active");
+		$(".menu-toggle > .fa-server, .menu-toggle > .fa-window-close").toggleClass("fa-server fa-window-close");
+		$(this).toggleClass("active");
+	});
+
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	$('#sidebar-wrapper .js-scroll-trigger').click(function() {
+		$("#sidebar-wrapper").removeClass("active");
+		$(".menu-toggle").removeClass("active");
+		$(".menu-toggle > .fa-server, .menu-toggle > .fa-window-close").toggleClass("fa-server fa-window-close");
+	});
+
+	$(document).scroll(function() {
+		var scrollDistance = $(this).scrollTop();
+		if (scrollDistance > 100) {
+			$('.scroll-to-top').fadeIn();
+		} else {
+			$('.scroll-to-top').fadeOut();
+		}
+	});
+
+})(jQuery);
+
+//Slide Animation
+$(document).ready(function() {
+	$(window).scroll(function() {
+		$(".slideanim").each(function() {
+			var pos = $(this).offset().top;
+
+			var winTop = $(window).scrollTop();
+			if (pos < winTop + 600) {
+				$(this).addClass("slide");
+			}
+		});
+	});
+})
